bb1e5e29da5f443a2bb8c8e8cf2f99863c52486c
sampletext32 <sampletext32@bk.ru>
2020-04-24 21:02:12 +0300

    Add project

---
+++ Prohorenko_1/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	cout << "Программу сделала Прохоренко Полина Андреевна студент группы УТН-111\n";
+	cout << "Программа для нахождения трёхзначных чисел с заданной суммой цифр\n";
+
+	int k;
+
+	do
+	{
+		int n;
+		cout << "Введите число N: ";
+		cin >> n;
+		int count = 0;
+
+		for (int i = 100; i < 1000; i++)
+		{
+			int sum = 0;
+			int t = i;
+			while (t != 0)
+			{
+				sum += t % 10;
+				t /= 10;
+			}
+			if (sum == n)
+			{
+				count++;
+				cout << i << " ";
+			}
+		}
+
+		cout << "\nНайдено " << count << " трёхзначных чисел с суммой цифр " << n << "\n";
+		cout << "Повторить(1): ";
+		cin >> k;
+	}
+	while (k == 1);
+
+	system("pause");
+
+	return 0;
+}
